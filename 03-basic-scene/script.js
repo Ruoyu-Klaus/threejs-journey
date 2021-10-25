4ecd3b300e02a569f45266e089904c6c7211fecd
RuoyuWang <klaus1201810802@gmail.com>
2021-10-25 15:58:01 +0800

    🚀 drawing first cube

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,18 +1,28 @@
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-const sizes = {
+const size = {
   width: 800,
   height: 600,
 };
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cubeMesh = new THREE.Mesh(geometry, material);
+scene.add(cubeMesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  size.width / size.height,
+  1,
+  1000
+);
+
 camera.position.z = 3;
 scene.add(camera);
-const canvas = document.querySelector("canvas.webgl");
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
+
+//Renderer
+const canvas = document.querySelector('canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(size.width, size.height);
 renderer.render(scene, camera);
